8eaa8ca1de4d0682757818326df24aa547b67387
gracelandolfi <glandolfi@icloud.com>
2024-11-28 00:34:08 -0500

    Initial Commit

---
+++ FilmTrack/FilmTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmTrackApp.swift
+//  FilmTrack
+//
+//  Created by Grace Landolfi on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FilmTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
