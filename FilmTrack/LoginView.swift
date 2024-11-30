dee561ce763eed352a4c7b8cdb48d1172d97ede0
gracelandolfi <glandolfi@icloud.com>
2024-11-29 19:35:23 -0500

    v1.3

---
+++ FilmTrack/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  FilmTrack
+//
+//  Created by Grace Landolfi on 11/29/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
